e69b07502f922c06983d63da61c8938851b7e3fd
srshahare <srshahare22.dev@gmail.com>
2023-04-21 02:35:21 +0530

    done

--- domain-mapper-client/src/utils/baseUrl.ts
+++ domain-mapper-client/src/utils/baseUrl.ts
@@ -1,5 +1,6 @@
 // const baseUrl = "http://localhost:5000"
-const baseUrl = "https://mktapi.com"
+// const baseUrl = "https://mktapi.com"
+const baseUrl = "https://68.183.244.172:5000"
 
 
 export default baseUrl;
